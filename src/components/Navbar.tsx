78e2b6a665a5f884455c71dfaaf95f442a9b79ae
Vineeth-0509 <vineeththungani@gmail.com>
2025-09-29 10:50:23 +0530

    navbar user info dropdown menu details with signout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,22 +2,27 @@ import { getAuthSession } from "@/lib/nextauth";
 import Link from "next/link";
 import React from "react";
 import SignInButton from "./SignInButton";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
   const session = await getAuthSession();
-  console.log(session?.user);
+
   return (
     <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl ">
         {/* Logo */}
-        <Link href='/' className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2">
           <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:translate-y-[2px] md:block dark:border-white">
-AI Quiz
+            AI Quiz
           </p>
         </Link>
-  <div className="flex items-center">
-    <SignInButton text="Sign In" />
-    </div>        
+        <div className="flex items-center">
+          {session?.user ? (
+            <UserAccountNav user={session.user}/>
+          ) : (
+            <SignInButton text="Sign In" />
+          )}
+        </div>
       </div>
     </div>
   );
