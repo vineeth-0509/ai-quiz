0766ad16a4d876a95d999814f790b19bd9adee16
Vineeth-0509 <vineeththungani@gmail.com>
2025-09-29 10:08:37 +0530

    navbar/layout.ts global available and with the google authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,26 @@
-import { getAuthSession } from '@/lib/nextauth';
-import React from 'react'
-
-
+import { getAuthSession } from "@/lib/nextauth";
+import Link from "next/link";
+import React from "react";
+import SignInButton from "./SignInButton";
 
 const Navbar = async () => {
   const session = await getAuthSession();
-  if(session?.user){
-    return <pre>{JSON.stringify(session.user, null, 2)}</pre>
-  }else{
-    return (
-        <div>
-            Not Signed In
-        </div>
-    )
-  }
-   
-}
+  console.log(session?.user);
+  return (
+    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
+      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl ">
+        {/* Logo */}
+        <Link href='/' className="flex items-center gap-2">
+          <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:translate-y-[2px] md:block dark:border-white">
+AI Quiz
+          </p>
+        </Link>
+  <div className="flex items-center">
+    <SignInButton text="Sign In" />
+    </div>        
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -16,7 +16,43 @@ declare module "next-auth/jwt"{
         id: string;
     }
 }
-export const authOptions : NextAuthOptions = {
+// export const authOptions : NextAuthOptions = {
+//     session:{
+//         strategy:"jwt"
+//     },
+//     callbacks:{
+//         jwt: async ({token})=>{
+//             const db_user = await prisma.user.findFirst({
+//                 where:{
+//                     email: token?.email
+//                 }
+//             })
+//             if(db_user){
+//                 token.id = db_user.id;
+//             }
+//             return token;
+//         },
+//         session: ({session, token})=>{
+//             if(token){
+//                 session.user.id = token.id as string;
+//                 session.user.name = token.name as string;
+//                 session.user.email = token.email as string;
+//                 session.user.image = token.image as string;
+//             }
+//             return session;
+//         }
+//     },
+//     secret: process.env.NEXTAUTH_SECRET,
+//     adapter: PrismaAdapter(prisma), 
+//     providers:[
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_CLIENT_ID as string,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+//         })
+//     ]
+// }
+
+export const authOptions: NextAuthOptions = {
     session:{
         strategy:"jwt"
     },
@@ -28,9 +64,9 @@ export const authOptions : NextAuthOptions = {
                 }
             })
             if(db_user){
-                token.id = db_user.id;
-            }
-            return token;
+            token.id = db_user.id;
+        }
+        return token;
         },
         session: ({session, token})=>{
             if(token){
@@ -41,9 +77,10 @@ export const authOptions : NextAuthOptions = {
             }
             return session;
         }
+        
     },
-    secret: process.env.NEXT_AUTH_SECRET,
-    adapter: PrismaAdapter(prisma), 
+    secret: process.env.NEXTAUTH_SECRET,
+    adapter:PrismaAdapter(prisma),
     providers:[
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -51,7 +88,6 @@ export const authOptions : NextAuthOptions = {
         })
     ]
 }
-
 
 export const getAuthSession = ()=>{
     return getServerSession(authOptions);
